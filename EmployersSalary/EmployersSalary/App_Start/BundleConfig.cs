c038583d53fac92bb99fab32c2abccfa4ab7495d
taintless <jokubas.raila@gmail.com>
2017-02-14 22:16:28 +0200

    Added theme

--- EmployersSalary/EmployersSalary/App_Start/BundleConfig.cs
+++ EmployersSalary/EmployersSalary/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace EmployersSalary
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-sandstone.css",
                       "~/Content/site.css"));
         }
     }
