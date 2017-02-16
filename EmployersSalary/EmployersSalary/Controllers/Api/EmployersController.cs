26ebe5b20af5ff650217d1f660441c5624845e7b
taintless <jokubas.raila@gmail.com>
2017-02-16 17:50:42 +0200

    Basic tasks completed

---
+++ EmployersSalary/EmployersSalary/Controllers/Api/EmployersController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using EmployersSalary.Models;
+
+namespace EmployersSalary.Controllers.Api
+{
+    public class EmployersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public EmployersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/employers
+        [Route("api/employers")]
+        public IHttpActionResult GetEmployers()
+        {
+            var employers = _context.Employers;
+
+            return Ok(employers);
+        }
+
+        // GET /api/employers/firstName/lastName
+        [Route("api/employer")]
+        public IHttpActionResult GetEmployer([FromUri] string firstName, string lastName)
+        {
+            var employer = _context.Employers.SingleOrDefault(c => c.FirstName == firstName && c.LastName == lastName);
+
+            if (employer == null)
+                return NotFound();
+
+            return Ok(employer);
+        }
+
+        // POST /api/employers
+        [HttpPost]
+        public IHttpActionResult CreateEmployer(Employer employer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Employers.Add(employer);
+            _context.SaveChanges();
+
+
+            return Created(new Uri(Request.RequestUri + "/" + employer.FirstName + "/" + employer.LastName), employer);
+        }
+
+        // PUT /api/employers/1
+        [HttpPut]
+        [Route("api/employers")]
+        public IHttpActionResult UpdateEmployer([FromUri] string firstName, string lastName, Employer employer)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var employerInDb = _context.Employers.SingleOrDefault(c => c.FirstName == firstName && c.LastName == lastName);
+
+            if (employerInDb == null)
+                return NotFound();
+
+            employerInDb.FirstName = employer.FirstName;
+            employerInDb.LastName = employer.LastName;
+            employerInDb.NetSalary = employer.NetSalary;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE /api/employers/1
+        [HttpDelete]
+        [Route("api/employers")]
+        public IHttpActionResult DeleteEmployer([FromUri] string firstName, string lastName)
+        {
+            var employerInDb = _context.Employers.SingleOrDefault(c => c.FirstName == firstName && c.LastName == lastName);
+
+            if (employerInDb == null)
+                return NotFound();
+
+            _context.Employers.Remove(employerInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ EmployersSalary/EmployersSalary/Controllers/EmployersController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using EmployersSalary.Models;
+
+namespace EmployersSalary.Controllers
+{
+    public class EmployersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EmployersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            //if (User.IsInRole(RoleName.CanManageMovies))
+            return View("List");
+
+            //return View("ReadOnlyList");
+        }
+
+        //public ActionResult New()
+        //{
+        //    var employer = new Employer();
+        //    return View("EmployerForm", employer);
+        //}
+
+        [System.Web.Mvc.HttpPost]
+        public ActionResult Save(Employer employer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new Employer
+                {
+                    FirstName = employer.FirstName,
+                    LastName = employer.LastName,
+                    NetSalary = employer.NetSalary
+                };
+
+                return View("EmployerForm", viewModel);
+            }
+
+            var employerInDb =
+                _context.Employers.Single(e => e.FirstName == employer.FirstName && e.LastName == employer.LastName);
+            employerInDb.NetSalary = employer.NetSalary;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Employers");
+        }
+
+       // [Route("/employers/{firstName}/{lastName}")]
+        public ActionResult Edit([FromUri] string firstName, string lastName)
+        {
+
+            var employer = _context.Employers.SingleOrDefault(c => c.FirstName == firstName && c.LastName == lastName);
+            if (employer == null)
+                return HttpNotFound();
+
+            return View("EmployerForm", employer);
+        }
+    }
+}
