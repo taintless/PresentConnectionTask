65fc94153f87f3f5dc7368aad926596dd02255c6
taintless <jokubas.raila@gmail.com>
2017-04-02 11:43:22 +0300

    React example

--- EmployersSalary/EmployersSalary/Controllers/HomeController.cs
+++ EmployersSalary/EmployersSalary/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployersSalary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,32 @@ namespace EmployersSalary.Controllers
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is another comment"
+                },
+            };
+        }
         public ActionResult Index()
         {
             return View();
@@ -27,6 +54,26 @@ namespace EmployersSalary.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ReactIndex()
+        {
+            return View();
+        }
+
+        [OutputCache(Location = System.Web.UI.OutputCacheLocation.None)]
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(CommentModel comment)
+        {
+            // Create a fake ID for this comment
+            comment.Id = _comments.Count + 1;
+            _comments.Add(comment);
+            return Content("Success :)");
         }
     }
 }
