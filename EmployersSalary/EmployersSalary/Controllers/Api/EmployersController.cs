59e458b11119a0ba573fc96de905dd911d2ce708
taintless <jokubas.raila@gmail.com>
2017-04-03 06:10:16 +0300

    no message

--- EmployersSalary/EmployersSalary/Controllers/Api/EmployersController.cs
+++ EmployersSalary/EmployersSalary/Controllers/Api/EmployersController.cs
@@ -94,5 +94,38 @@ namespace EmployersSalary.Controllers.Api
 
             return Ok();
         }
+        private static readonly IList<CommentModel> _comments;
+
+        static EmployersController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "This is another comment"
+                },
+            };
+        }
+        [AllowAnonymous]
+        [Route("api/employers/Comments")]
+        //[OutputCache(Location = System.Web.UI.OutputCacheLocation.None)]
+        public IHttpActionResult Comments()
+        {
+            return Ok(_comments);
+        }
     }
 }
